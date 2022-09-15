354112bdab74c62e4b2d3acb0b95fc1ced5a7441
MakarovOleg1988 <makarov.oleg1988@yandex.ru>
2022-09-15 15:40:15 +0300

    Исправления в строке 27

---
+++ Lesson10/Lesson10.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Lesson10
+{
+    class Lesson10
+    {
+        static void Main(string[] args)
+        {
+            MyMethod();
+        }
+
+            public static void MyMethod()
+            {
+                int _posY = 12;
+                string name = "Bill";
+                bool _isRun = true;
+
+                double _speed = 12.3;
+                int _year = 22;
+                string team = "WinnerTeam";
+                //
+                if (_posY < 10 && name[0] == 'B' || !_isRun)
+                {
+                    Console.WriteLine(true + 1.ToString()); //false
+                }
+
+                if (team == "WinnerTeAM" || !(name == "Dave" && _posY < 1000 || _isRun == true))
+                {
+                    Console.WriteLine(true + 2.ToString());//false
+                }
+
+                if (_year > 22 && _year != 22 || _speed > 12 && _speed < 13)
+                {
+                    Console.WriteLine(true + 3.ToString());//true
+                }
+
+                if (_isRun || (!_isRun || _isRun) || _isRun && _isRun)
+                {
+                    Console.WriteLine(true + 4.ToString());//true
+                }
+            }
+      
+    }
+}
